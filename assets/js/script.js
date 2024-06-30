d7d1c8b58e0b2f870bc669c5a073113c65abdaf7
RoniCee <ronancondon@yahoo.com>
2024-06-30 20:26:47 +0000

    updte

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log("connected")
+// Wait for the DOM to finsih loading before running the game
+
+document.addEventListener("DOMContentLoaded", function()    {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit")    {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert("You clicked ${gameType}");
+            }
+        })
+    }
+})
+
+function runGame()  {
+
+}
+function checkAnswer()  {
+
+}
+function calculateCorrectAnswer()   {
+
+}
+function incrementScore()   {
+
+}
+
+function displayAdditionQuestion()   {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+function displayMultiplyQuestion()  {
+
+}
+
